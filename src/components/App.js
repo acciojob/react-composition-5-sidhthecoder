2d7922a02e0f1631cff2c8563b1d5ad8d6e035c2
Siddhant Maurya <siddhant.maurya.1122@gmail.com>
2023-07-21 13:02:24 +0530

    submitting code-Fri Jul 21 2023 13:02:22 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import Tabs from "./Tabs";
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <Tabs/>
     </div>
   )
 }
---
+++ src/components/Tabs.js
@@ -0,0 +1,31 @@
+import React,{useState} from "react";
+
+const Tabs=()=>{
+    let [numtab,setNumtab]=useState("");
+
+    function handleFnc(e){
+     setNumtab(e.target.dataset.tab);
+    }
+    let[lettertab,setLettertab]=useState("");
+    function handleFnc1(e){
+        setLettertab(e.target.dataset.tab);
+    }
+    return (
+        <div>
+            <ul>
+               <li data-tab="Tab 1" onMouseOver={handleFnc}>Tab 1</li>
+               <li data-tab="Tab 2" onMouseOver={handleFnc}>Tab 2</li>
+               <li data-tab="Tab 3" onMouseOver={handleFnc}>Tab 3</li>
+            </ul>
+            <p>Content for {numtab}</p>
+            <ul>
+            <li data-tab="Tab A" onMouseOver={handleFnc1}>Tab A</li>
+               <li data-tab="Tab B" onMouseOver={handleFnc1}>Tab B</li>
+               <li data-tab="Tab C" onMouseOver={handleFnc1}>Tab C</li>
+            </ul>
+            <p>Content for {lettertab}</p>
+        </div>
+    )
+}
+
+export default Tabs;
